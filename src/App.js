912655428a45e9edd328db341a679aa8639deb07
Lucas Santos <lucasantos112291@gmail.com>
2023-01-28 13:36:16 +0000

    lab done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import foods from './foods.json';
+import { useState } from 'react';
+import FoodBox from './component/FoodBox';
+import AddFoodForm from './component/AddFoodForm';
+import SearchBar from './component/SearchBar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+  function App() {
+    const [itens, setItens] = useState(foods);
+    const [showFood, setShowFood] = useState(foods);
+
+    const createFood = (newFood) => {
+      const food = [newFood, ...itens]
+      setItens(food)
+      setShowFood(food)
+    }
+
+    const filterFood = (searchQuery) => {
+
+      let filteredFood = itens.filter((food) =>
+        food.name.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+
+      setShowFood(filteredFood);
+    }; 
+
+
+    const deleteFood = (foodName) => {
+
+      let deletedFood = itens.filter((food) => food.name !== foodName)
+      
+      setItens(deletedFood)
+      setShowFood(deletedFood)
+    }; 
+
+    return (
+
+      <div className="App">
+      <SearchBar filterFood = {filterFood}/>
+      <AddFoodForm createFood={createFood}/>
+
+        {showFood.map((food) => {
+          return (
+            <div>
+              <FoodBox food = {food} deleteFood={deleteFood}/>
+            </div>
+          );
+        })}
+
+        <SearchBar filterFood={filterFood} />
+
+
+      </div>
+
+
+    );
+  }
+
+  export default App;
